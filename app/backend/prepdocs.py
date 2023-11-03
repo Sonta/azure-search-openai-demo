257612e0216a3d683263f431f6de263bb69c6348
sonta <son.ta.dinh@gmail.com>
2023-11-03 15:42:04 +0100

    fix: adjust prepdocs script

--- app/backend/prepdocs.py
+++ app/backend/prepdocs.py
@@ -1,10 +1,12 @@
 import argparse
 import asyncio
+import os
 from typing import Any, Optional, Union
 
 from azure.core.credentials import AzureKeyCredential
 from azure.core.credentials_async import AsyncTokenCredential
 from azure.identity.aio import AzureDeveloperCliCredential
+from dotenv import load_dotenv
 from prepdocslib.blobmanager import BlobManager
 from prepdocslib.embeddings import (
     AzureOpenAIEmbeddingService,
@@ -21,13 +23,15 @@ from prepdocslib.pdfparser import DocumentAnalysisPdfParser, PdfParser
 from prepdocslib.strategy import SearchInfo, Strategy
 from prepdocslib.textsplitter import TextSplitter
 
+load_dotenv()
+
 
 def is_key_empty(key):
     return key is None or len(key.strip()) == 0
 
 
-def setup_file_strategy(credential: AsyncTokenCredential, args: Any) -> FileStrategy:
-    storage_creds = credential if is_key_empty(args.storagekey) else args.storagekey
+def setup_file_strategy(args: Any) -> FileStrategy:
+    storage_creds = args.storagekey
     blob_manager = BlobManager(
         endpoint=f"https://{args.storageaccount}.blob.core.windows.net",
         container=args.container,
@@ -42,11 +46,7 @@ def setup_file_strategy(credential: AsyncTokenCredential, args: Any) -> FileStra
             "Error: Azure Form Recognizer service is not provided. Please provide formrecognizerservice"
         )
         exit(1)
-    formrecognizer_creds: Union[AsyncTokenCredential, AzureKeyCredential] = (
-        credential
-        if is_key_empty(args.formrecognizerkey)
-        else AzureKeyCredential(args.formrecognizerkey)
-    )
+    formrecognizer_creds = AzureKeyCredential(args.formrecognizerkey)
     pdf_parser = DocumentAnalysisPdfParser(
         endpoint=f"https://{args.formrecognizerservice}.cognitiveservices.azure.com/",
         credential=formrecognizer_creds,
@@ -56,11 +56,9 @@ def setup_file_strategy(credential: AsyncTokenCredential, args: Any) -> FileStra
     use_vectors = not args.novectors
     embeddings: Optional[OpenAIEmbeddings] = None
     if use_vectors and args.openaihost != "openai":
-        azure_open_ai_credential: Union[AsyncTokenCredential, AzureKeyCredential] = (
-            credential
-            if is_key_empty(args.openaikey)
-            else AzureKeyCredential(args.openaikey)
-        )
+        azure_open_ai_credential: Union[
+            AsyncTokenCredential, AzureKeyCredential
+        ] = AzureKeyCredential(os.getenv("OPENAI_API_KEY"))
         embeddings = AzureOpenAIEmbeddingService(
             open_ai_service=args.openaiservice,
             open_ai_deployment=args.openaideployment,
@@ -69,34 +67,14 @@ def setup_file_strategy(credential: AsyncTokenCredential, args: Any) -> FileStra
             disable_batch=args.disablebatchvectors,
             verbose=args.verbose,
         )
-    elif use_vectors:
-        embeddings = OpenAIEmbeddingService(
-            open_ai_model_name=args.openaimodelname,
-            credential=args.openaikey,
-            organization=args.openaiorg,
-            disable_batch=args.disablebatchvectors,
-            verbose=args.verbose,
-        )
 
     print("Processing files...")
     list_file_strategy: ListFileStrategy
-    if args.datalakestorageaccount:
-        adls_gen2_creds = (
-            credential if is_key_empty(args.datalakekey) else args.datalakekey
-        )
-        print(f"Using Data Lake Gen2 Storage Account {args.datalakestorageaccount}")
-        list_file_strategy = ADLSGen2ListFileStrategy(
-            data_lake_storage_account=args.datalakestorageaccount,
-            data_lake_filesystem=args.datalakefilesystem,
-            data_lake_path=args.datalakepath,
-            credential=adls_gen2_creds,
-            verbose=args.verbose,
-        )
-    else:
-        print(f"Using local files in {args.files}")
-        list_file_strategy = LocalListFileStrategy(
-            path_pattern=args.files, verbose=args.verbose
-        )
+
+    print(f"Using local files in {args.files}")
+    list_file_strategy = LocalListFileStrategy(
+        path_pattern=args.files, verbose=args.verbose
+    )
 
     if args.removeall:
         document_action = DocumentAction.RemoveAll
@@ -118,12 +96,8 @@ def setup_file_strategy(credential: AsyncTokenCredential, args: Any) -> FileStra
     )
 
 
-async def main(strategy: Strategy, credential: AsyncTokenCredential, args: Any):
-    search_creds: Union[AsyncTokenCredential, AzureKeyCredential] = (
-        credential
-        if is_key_empty(args.searchkey)
-        else AzureKeyCredential(args.searchkey)
-    )
+async def main(strategy: Strategy, args: Any):
+    search_creds = AzureKeyCredential(args.searchkey)
     search_info = SearchInfo(
         endpoint=f"https://{args.searchservice}.search.windows.net/",
         credential=search_creds,
@@ -144,45 +118,25 @@ if __name__ == "__main__":
     )
     parser.add_argument("files", nargs="?", help="Files to be processed")
     parser.add_argument(
-        "--datalakestorageaccount",
-        required=False,
-        help="Optional. Azure Data Lake Storage Gen2 Account name",
-    )
-    parser.add_argument(
-        "--datalakefilesystem",
-        required=False,
-        default="gptkbcontainer",
-        help="Optional. Azure Data Lake Storage Gen2 filesystem name",
-    )
-    parser.add_argument(
-        "--datalakepath",
-        required=False,
-        help="Optional. Azure Data Lake Storage Gen2 filesystem path containing files to index. If omitted, index the entire filesystem",
-    )
-    parser.add_argument(
-        "--datalakekey",
-        required=False,
-        help="Optional. Use this key when authenticating to Azure Data Lake Gen2",
-    )
-    parser.add_argument(
         "--useacls",
         action="store_true",
-        help="Store ACLs from Azure Data Lake Gen2 Filesystem in the search index",
+        help="Skip uploading individual pages to Azure Blob Storage",
     )
+    parser.add_argument(
+        "--storageaccount",
+        default="dtfsdocumentchatstorage",
+        help="Azure Blob Storage account name",
+    ),
     parser.add_argument(
         "--category",
         help="Value for the category field in the search index for all sections indexed in this run",
     )
     parser.add_argument(
-        "--skipblobs",
-        action="store_true",
-        help="Skip uploading individual pages to Azure Blob Storage",
+        "--container", default="devcontent", help="Azure Blob Storage container name"
     )
-    parser.add_argument("--storageaccount", help="Azure Blob Storage account name")
-    parser.add_argument("--container", help="Azure Blob Storage container name")
     parser.add_argument(
         "--storagekey",
-        required=False,
+        default=os.getenv("BLOB_STORAGE_KEY"),
         help="Optional. Use this Azure Blob Storage account key instead of the current user identity to login (use az login to set current user for Azure)",
     )
     parser.add_argument(
@@ -192,15 +146,17 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--searchservice",
+        default=os.getenv("AZURE_SEARCH_SERVICE"),
         help="Name of the Azure Cognitive Search service where content should be indexed (must exist already)",
     )
     parser.add_argument(
         "--index",
+        default=os.getenv("AZURE_SEARCH_INDEX"),
         help="Name of the Azure Cognitive Search index where content should be indexed (will be created if it doesn't exist)",
     )
     parser.add_argument(
         "--searchkey",
-        required=False,
+        default=os.getenv("AZURE_COGNITIVE_SEARCH_ADMIN_KEY"),
         help="Optional. Use this Azure Cognitive Search account key instead of the current user identity to login (use az login to set current user for Azure)",
     )
     parser.add_argument(
@@ -211,18 +167,22 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--openaihost",
+        default="azure",
         help="Host of the API used to compute embeddings ('azure' or 'openai')",
     )
     parser.add_argument(
         "--openaiservice",
+        default="DTFS-DocumentChat-OpenAI-Sweden",
         help="Name of the Azure OpenAI service used to compute embeddings",
     )
     parser.add_argument(
         "--openaideployment",
+        default="ada2",
         help="Name of the Azure OpenAI model deployment for an embedding model ('text-embedding-ada-002' recommended)",
     )
     parser.add_argument(
         "--openaimodelname",
+        default="text-embedding-ada-002",
         help="Name of the Azure OpenAI embedding model ('text-embedding-ada-002' recommended)",
     )
     parser.add_argument(
@@ -236,16 +196,6 @@ if __name__ == "__main__":
         help="Don't compute embeddings in batch for the sections",
     )
     parser.add_argument(
-        "--openaikey",
-        required=False,
-        help="Optional. Use this Azure OpenAI account key instead of the current user identity to login (use az login to set current user for Azure). This is required only when using non-Azure endpoints.",
-    )
-    parser.add_argument(
-        "--openaiorg",
-        required=False,
-        help="This is required only when using non-Azure endpoints.",
-    )
-    parser.add_argument(
         "--remove",
         action="store_true",
         help="Remove references to this document from blob storage and the search index",
@@ -257,26 +207,20 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--formrecognizerservice",
-        required=False,
+        default="dtfs-document-intelligence",
         help="Optional. Name of the Azure Form Recognizer service which will be used to extract text, tables and layout from the documents (must exist already)",
     )
     parser.add_argument(
         "--formrecognizerkey",
-        required=False,
+        default=os.getenv("AZURE_DOCUMENT_INTELLIGENCE_KEY"),
         help="Optional. Use this Azure Form Recognizer account key instead of the current user identity to login (use az login to set current user for Azure)",
     )
 
     parser.add_argument("--verbose", "-v", action="store_true", help="Verbose output")
     args = parser.parse_args()
 
-    # Use the current user identity to connect to Azure services unless a key is explicitly set for any of them
-    azd_credential = (
-        AzureDeveloperCliCredential()
-        if args.tenantid is None
-        else AzureDeveloperCliCredential(tenant_id=args.tenantid, process_timeout=60)
-    )
-
-    file_strategy = setup_file_strategy(azd_credential, args)
+    print(args)
+    file_strategy = setup_file_strategy(args)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(file_strategy, azd_credential, args))
+    loop.run_until_complete(main(file_strategy, args))
     loop.close()
